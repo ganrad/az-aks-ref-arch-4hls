1ae30944237d65cf75431032c70f3963fa1a0697
Ganesh Radhakrishnan <ganrad01@gmail.com>
2021-06-09 18:43:05 +0000

    Updated scripts

--- dev-cluster/azure-cli/deploy.sh
+++ dev-cluster/azure-cli/deploy.sh
@@ -1,6 +1,9 @@
 #
-#
-#
+# Author: Ganesh Radhakrishnan@Microsoft
+# Email: ganrad01@gmail.com
+# Date: 06-09-2021
+# Description: Use this script to provision a 'dev' AKS cluster
+# Notes:
 #
 #---------------------------------------------------
 # Variables
@@ -9,7 +12,7 @@
 LOCATION="westus"
 RESOURCE_GROUP="csu-dev-grts-dc"
 
-# Virtual network and subnets
+# ***** Virtual network and subnets
 VNET_NAME="devVnet"
 VNET_ADDRESS_CIDR="192.168.0.0/16"
 SERVICES_SUBNET_NAME="services-subnet"
@@ -19,7 +22,7 @@ BASTION_SUBNET_CIDR="192.168.2.0/24"
 AKS_SUBNET_NAME="aks-subnet"
 AKS_SUBNET_CIDR="192.168.3.0/24"
 
-# Azure VM's
+# ***** Azure VM's
 JUMP_VM_NAME="k8s-lab-vm"
 JUMP_VM_NIC_NAME="k8s-lab-vm-nic"
 JUMP_VM_IMAGE="Debian:debian-10:10:latest"
@@ -28,11 +31,43 @@ JUMP_VM_DISK_SIZE=128
 JUMP_VM_ADMIN_UNAME="labuser"
 JUMP_VM_ADMIN_PWD="azk8sLab2021!"
 
-# Bastion host
+# ***** Bastion host
 BASTION_HOST_NAME="bastionHost"
 BASTION_HOST_PIP_NAME="bastionPublicIP"
+
+# ***** Azure Container Registry 
+# ACR name must be unique and can contain 5-50 alphanumeric characters
+ACR_NAME="k8sdevRegistry01"
+# SKU = Basic, Standard, Premium
+ACR_SKU="Standard"
+
+# ***** Azure Kubernetes Service
+AKS_NAME="k8sdevlab"
+AKS_NODE_COUNT=3
+AKS_CNI_PLUGIN="kubenet"
+AKS_SERVICE_CIDR="10.0.0.0/16"
+AKS_DNS_SERVICE_IP="10.0.0.10"
+# Pod cidr is only needed for 'kubenet' CNI plug-in
+# If using Azure CNI plug-in remember to update the AKS create command below
+AKS_POD_CIDR="10.244.0.0/16"
+AKS_NODE_COUNT_MIN=3
+AKS_NODE_COUNT_MAX=5
+AKS_NODE_VM_SIZE="Standard_DS2_v2"
+# Use OS disk type 'Ephemeral' for low read/write latency (/io)
+AKS_NODE_OS_DISK_TYPE="Ephemeral"
+# Disk size is in GB
+AKS_NODE_OS_DISK_SIZE="60"
+AKS_TAGS="env=lab unit=cloud"
+AKS_NODEPOOL_TAGS="env=lab unit=cloud"
+# (Optional) For cluster with Windows and Linux nodepools, specify Windows user name and password
+AKS_WIN_ADM_UNAME="admin"
+AKS_WIN_ADM_PWD="Password2021!"
+AKS_LOAD_BALANCER_SKU="standard"
+# Always overcommit the no. of pods / node
+AKS_MAX_PODS=60
+
 #---------------------------------------------------
-# Script
+# ***** Script
 #---------------------------------------------------
 
 ## Exit when any command fails
@@ -47,6 +82,7 @@ az network vnet create --resource-group $RESOURCE_GROUP \
   --address-prefixes $VNET_ADDRESS_CIDR \
   --subnet-name $SERVICES_SUBNET_NAME \
   --subnet-prefix $SERVICES_SUBNET_CIDR
+VNET_ID=$(az network vnet show --resource-group $RESOURCE_GROUP --name $VNET_NAME --query id -o tsv)
 echo -e "Azure virtual network [$VNET_NAME] created\n"
 
 az network nic create \
@@ -90,7 +126,7 @@ az network bastion create --name $BASTION_HOST_NAME \
 echo -e "Bastion service [$BASTION_HOST_NAME] created\n"
 
 az vm extension set \
-  --name toolsScript \
+  --name customScript \
   --resource-group $RESOURCE_GROUP \
   --vm-name $JUMP_VM_NAME \
   --publisher Microsoft.Azure.Extensions \
@@ -100,5 +136,46 @@ az vm extension list \
   -g $RESOURCE_GROUP \
   --vm-name $JUMP_VM_NAME
 echo -e "Updated jump-box [$JUMP_VM_NAME] with pre-requisite tools - Azure CLI, k8s CLI & Helm CLI\n"
+
+az acr create --resource-group $RESOURCE_GROUP \
+  --name $ACR_NAME \
+  --sku $ACR_SKU
+echo -e "ACR [$ACR_NAME] created\n"
+
+az network vnet subnet create --name $AKS_SUBNET_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name $VNET_NAME \
+  --address-prefixes $AKS_SUBNET_CIDR
+AKS_SUBNET_ID=$(az network vnet subnet show --resource-group $RESOURCE_GROUP --vnet-name $VNET_NAME --name $AKS_SUBNET_NAME --query id -o tsv)
+echo -e "AKS subnet [$AKS_SUBNET_NAME] created\n"
+
+# Provision AKS with latest stable version
+az aks create --resource-group $RESOURCE_GROUP \
+  --name $AKS_NAME \
+  --node-count $AKS_NODE_COUNT \
+  --network-plugin $AKS_CNI_PLUGIN \
+  --service-cidr $AKS_SERVICE_CIDR \
+  --dns-service-ip $AKS_DNS_SERVICE_IP \
+  --pod-cidr $AKS_POD_CIDR \
+  --docker-bridge-address 172.17.0.1/16 \
+  --vnet-subnet-id $AKS_SUBNET_ID \
+  --enable-managed-identity \
+  --enable-cluster-autoscaler \
+  --min-count $AKS_NODE_COUNT_MIN \
+  --max-count $AKS_NODE_COUNT_MAX \
+  --node-vm-size $AKS_NODE_VM_SIZE \
+  --node-osdisk-type $AKS_NODE_OS_DISK_TYPE \
+  --node-osdisk-size $AKS_NODE_OS_DISK_SIZE \
+  --tags $AKS_TAGS \
+  --nodepool-tags $AKS_NODEPOOL_TAGS \
+  --windows-admin-username $AKS_WIN_ADM_UNAME \
+  --windows-admin-password $AKS_WIN_ADM_PWD \
+  --vm-set-type VirtualMachineScaleSets \
+  --load-balancer-sku $AKS_LOAD_BALANCER_SKU \
+  --max-pods $AKS_MAX_PODS \
+  --attach-acr $ACR_NAME \
+  --enable-addons http_application_routing,monitoring \
+  --zones 1 2 3
+echo -e "AKS [$AKS_NAME] created\n"
 
 echo -e "Finished deploying AKS dev cluster environment"
--- dev-cluster/azure-cli/install-script.sh
+++ dev-cluster/azure-cli/install-script.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
+#
+# --------------------------------------------------------
+# Author: Ganesh Radhakrishnan
+# Email : ganrad01@gmail.com
+# Date : 06-09-2021
+# Description: Use this script to install tools/utilities etc on the Linux jump-box
+# Notes:
+# --------------------------------------------------------
 
 set -e
-echo -e "Script execution started\n"
+echo -e "***** Script execution started *****\n"
 
 (echo n; echo p; echo 1; echo ; echo ; echo w) | sudo fdisk /dev/sdc
 
 sudo mkfs -t ext4 /dev/sdc1
 
-sudo mkdir -p /opt/local && sudo mount /dev/sdc1 /opt/local
+sudo mkdir -p $1 && sudo mount /dev/sdc1 $1
 
 UUID=$(sudo blkid -s UUID -o value /dev/sdc1)
 
-echo "UUID=$UUID	/opt/local	ext4	defaults,nofail	1	2" | sudo tee -a /etc/fstab
+echo "UUID=$UUID	$1	ext4	defaults,nofail	1	2" | sudo tee -a /etc/fstab
 
-echo -e "Formatted disk and mounted file system\n"
+echo -e "***** Formatted disk and mounted file system @ [$1] *****\n"
 
 # Install Git
 sudo apt-get install -y git
-echo -e "Installed Git\n"
+echo -e "***** Installed Git *****\n"
 
 # Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-echo -e "Installed Azure CLI\n"
+echo -e "***** Installed Azure CLI *****\n"
 
 # Install Helm (Living on the edge ...)
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-echo -e "Installed Helm CLI\n"
+echo -e "***** Installed Helm CLI *****\n"
 
 # Install Kubernetes CLI (Latest version)
 sudo curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl && sudo chmod 755 /usr/local/bin/kubectl
-echo -e "Installed Kubernetes CLI\n"
+echo -e "***** Installed Kubernetes CLI *****\n"
 
-echo -e "Script execution finished\n"
+# Install additional tools as required (below) eg., docker engine, Istio service mesh etc
+
+echo -e "***** Script execution finished *****\n"
 
