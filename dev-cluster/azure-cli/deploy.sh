51a2eb26d9d0fbfad931e0caab5dff0428a96582
Ganesh Radhakrishnan <ganrad01@gmail.com>
2021-06-09 15:45:49 +0000

    Debugging 1.

--- dev-cluster/azure-cli/deploy.sh
+++ dev-cluster/azure-cli/deploy.sh
@@ -4,6 +4,7 @@
 #
 #---------------------------------------------------
 # Variables
+#---------------------------------------------------
 #
 LOCATION="westus"
 RESOURCE_GROUP="csu-dev-grts-dc"
@@ -30,6 +31,9 @@ JUMP_VM_ADMIN_PWD="azk8sLab2021!"
 # Bastion host
 BASTION_HOST_NAME="bastionHost"
 BASTION_HOST_PIP_NAME="bastionPublicIP"
+#---------------------------------------------------
+# Script
+#---------------------------------------------------
 
 ## Exit when any command fails
 set -e
@@ -84,5 +88,17 @@ az network bastion create --name $BASTION_HOST_NAME \
   --vnet-name $VNET_NAME \
   --location $LOCATION
 echo -e "Bastion service [$BASTION_HOST_NAME] created\n"
+
+az vm extension set \
+  --name toolsScript \
+  --resource-group $RESOURCE_GROUP \
+  --vm-name $JUMP_VM_NAME \
+  --publisher Microsoft.Azure.Extensions \
+  --settings ./script-config.json
+
+az vm extension list \
+  -g $RESOURCE_GROUP \
+  --vm-name $JUMP_VM_NAME
+echo -e "Updated jump-box [$JUMP_VM_NAME] with pre-requisite tools - Azure CLI, k8s CLI & Helm CLI\n"
 
 echo -e "Finished deploying AKS dev cluster environment"
