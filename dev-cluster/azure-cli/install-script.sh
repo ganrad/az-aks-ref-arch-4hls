d6fcf4e3cca7f3ec367429d4d078f05868761d35
Ganesh Radhakrishnan <ganrad01@gmail.com>
2021-06-17 01:23:21 +0000

    Updated install method for azure CLI

--- dev-cluster/azure-cli/install-script.sh
+++ dev-cluster/azure-cli/install-script.sh
@@ -30,7 +30,7 @@ echo -e "***** Installed Git *****\n"
 # Install Azure CLI (this method is prone to throwing exceptions !!)
 #curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
 
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
@@ -38,8 +38,8 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sud
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-sudo apt-get update
-sudo apt-get install azure-cli
+sudo apt-get -y update
+sudo apt-get -y install azure-cli
 echo -e "***** Installed Azure CLI *****\n"
 
 # Install Helm (Living on the edge ...)
